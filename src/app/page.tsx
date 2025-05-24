32b430702c562bcf58f41221bb78e342c88ddfb0
amiparadis250 <pishimweaime7@gmail.com>
2025-05-24 08:40:57 +0200

    will configure next js project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+
+      <p className="text-lg text-gray-700">Welcome to ai4rise</p>
+    </div>
+  );
+}
